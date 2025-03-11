b73d85bfd7e60d87df0394365136da2f0609c38a
favau <guitiar1296@gmail.com>
2025-03-11 17:59:38 +0900

    first commit

---
+++ src/main/java/com/fav/fav/FavApplication.java
@@ -0,0 +1,13 @@
+package com.fav.fav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fav/fav/FavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fav.fav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
